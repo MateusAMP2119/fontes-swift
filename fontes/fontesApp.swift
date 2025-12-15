b85d6d4cb808015999046910354c95140b2fc89c
Mateus Costa <105391128+MateusAMP2119@users.noreply.github.com>
2025-12-15 07:07:21 +0000

    Initial Commit

---
+++ fontes/fontesApp.swift
@@ -0,0 +1,17 @@
+//
+//  fontesApp.swift
+//  fontes
+//
+//  Created by Mateus Costa on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct fontesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fontesTests/fontesTests.swift
@@ -0,0 +1,17 @@
+//
+//  fontesTests.swift
+//  fontesTests
+//
+//  Created by Mateus Costa on 15/12/2025.
+//
+
+import Testing
+@testable import fontes
+
+struct fontesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
